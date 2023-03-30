065466a7c37953ef144f4cded4e9272f6d9611cf
Gabriel <gabriel.medeiros.2002@hotmail.com>
2023-03-30 06:09:01 -0300

    Adicinado metodo add.

--- src/com/projeto/listaencadeada/ListaEncadeada.java
+++ src/com/projeto/listaencadeada/ListaEncadeada.java
@@ -8,6 +8,23 @@ public class ListaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo); //Instancia o No com o conteudo
+
+        if(this.isEmpty()){ //Se referenciaEntrada == Null ele adiciona como primeiro No
+            referenciaEntrada = novoNo;
+            return; //Usada para terminar a funcao
+        }
+
+        No<T> noAuxiliar = referenciaEntrada; // Grava o primeiro No em uma variavel auxiliar
+
+        for(int i = 0; i < this.size()-1; i++){
+            noAuxiliar = noAuxiliar.getProximoNo(); //Percorre a Lista para ir ate o final e inserir o novoNo
+        }
+
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
     public int size(){
         int tamanhoLista = 0; //Define uma variavel para contar o tamanho da lista
         No<T> referenciaAux = referenciaEntrada;
@@ -29,7 +46,6 @@ public class ListaEncadeada<T> {
 
         return tamanhoLista;
     }
-
     public boolean isEmpty(){
         return referenciaEntrada == null ? true : false;
     }
