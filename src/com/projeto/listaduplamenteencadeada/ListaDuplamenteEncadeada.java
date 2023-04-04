827542ce0eddbdd778fc2d2102248fefa1c1345f
Gabriel <gabriel.medeiros.2002@hotmail.com>
2023-04-04 09:07:23 -0300

    Add()

--- src/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ src/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -18,6 +18,21 @@ public class ListaDuplamenteEncadeada<T> {
         return this.getNo(index).getConteudo();
     }
 
+    public void add(T elemento){
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+
+        if(primeiroNo == null){
+            primeiroNo = novoNo;
+        }
+        if(ultimoNo != null){
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo;
 
@@ -29,6 +44,6 @@ public class ListaDuplamenteEncadeada<T> {
     }
 
     public int size(){
-        return tamanhoLista;
+        return this.tamanhoLista;
     }
 }
